e42f855a4b5fc498681aa3ae308bf605200d5270
Chris Baker <electro@1939worldsfair.com>
2017-07-26 07:07:09 -0700

    moves some of the spline math around
    I was recomputing the spline for each tile even though they were all
    identical, so I moved the math out so it could get computed by the app
    before rendering. It's still messy, but much improved.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 
 import './App.css';
 
+import PointList from './PointList';
 import Tile from './Tile';
 
 class App extends Component {
@@ -18,7 +19,6 @@ class App extends Component {
   componentDidMount() {
     const svg = ReactDOM.findDOMNode(this.refs.svg);
     if (svg && svg.createSVGPoint) {
-      console.log("svg: " + svg);
       const tmpPt = svg.createSVGPoint();
       if (tmpPt) {
         this.setState({tmpPt});
@@ -36,31 +36,71 @@ class App extends Component {
     const {width, height} = this.props;
     
     var {pointLists} = this.state;
-    var points = pointLists[list];
+    var pointList = pointLists[list];
+      var points = pointList.points;
     var [oldX, oldY] = points[index];
 
     if (index === 0 || index === points.length - 1) {
       if (oldX === 0) {
-        points[0] = [0, newY / height];
-        points[points.length - 1] = [1, newY / height];
+        var y = newY / height;
+        pointList.movePoint(0, [0, y]);
+        pointList.movePoint(points.length - 1, [1, y]);
       } else if (oldY === 0) {
-        points[0] = [newX / width, 0];
-        points[points.length - 1] = [newX / width, 1];
+        var x = newX / width;
+        pointList.movePoint(0, [x, 0]);
+        pointList.movePoint(points.length - 1, [x, 1]);
       }
     } else {
-      points[index] = [newX / width, newY / height];
+      pointList.movePoint(index, [newX / width, newY / height]);
     }
 
     this.setState({pointLists});
   }
 
+  pathDescription(controlPoints) {
+    const {width, height} = this.props;
+    const [x1, y1] = controlPoints[0];
+    var result = "M " + (x1 * width) + "," + (y1 * height);
+    for (var i = 1; i < controlPoints.length; i += 3) {
+      const [x2, y2] = controlPoints[i];
+      const [x3, y3] = controlPoints[i+1];
+      const [x4, y4] = controlPoints[i+2];
+      result += " C " + (x2 * width) + "," + (y2 * height)
+          + " " + (x3 * width) + "," + (y3 * height) + " " + (x4 * width) + "," + (y4 * height);
+    }
+
+    return result;
+  }
+
+  computeCurveProps() {
+    const {pointLists, ui} = this.state;
+    const {curveColor, curveWidth, width, height, pointRadius, pointColor} = this.props;
+    return pointLists.map(pointList => {
+      const controlPoints = pointList.flattenedControlPoints();
+      return {
+        pointList,
+        width,
+        height,
+        pointRadius,
+        pointColor,
+        ui,
+        d: this.pathDescription(controlPoints),
+        stroke: curveColor,
+        strokeWidth: curveWidth,
+        fill: "none"
+      };
+    });
+  }
+
   render() {
     const {width, height} = this.props;
-    var tileProps = Object.assign({movePoint: (i, pt) => this.movePoint(i, pt)}, this.state, this.props);
+    var tileProps = Object.assign({movePoint: this.movePoint.bind(this)}, this.state, this.props);
 
     const tileAt = (x, y) => {
       const trans = "translate(" + x + "," + y + ")";
-      return (<Tile key={trans} transform={trans} {...tileProps} />);
+      return (
+        <Tile key={trans} transform={trans} curveProps={this.computeCurveProps()} {...tileProps} />
+      );
     };
 
     var children = [];
@@ -87,8 +127,8 @@ App.defaultProps = {
   curveColor: "rgb(16,16, 16)",
   pointRadius: 5,
   pointColor: "rgb(100,200,200)",
-  pointLists: [[[0, 0.25], [0.5, 0.65], [0.3, 0.75], [0.4, 0.25], [1, 0.25]],
-               [[0.25, 0], [0.3, 0.75], [0.5, 0.65], [0.9, 0.75], [0.25, 1]]]
+  pointLists: [new PointList([[0, 0.25], [0.5, 0.65], [0.3, 0.75], [0.4, 0.25], [1, 0.25]]),
+               new PointList([[0.25, 0], [0.3, 0.75], [0.5, 0.65], [0.9, 0.75], [0.25, 1]])]
 };
 
 export default App;
