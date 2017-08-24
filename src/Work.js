1e028f4c2ca859143a624b56d021978af5b01f3e
Chris Baker <electro@1939worldsfair.com>
2017-08-23 19:23:34 -0700

    splits width calculation from rest of work code

--- src/Work.js
+++ src/Work.js
@@ -16,13 +16,6 @@ import CurveProps from './curveProps';
 import Controls from './Controls';
 
 class Work extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: undefined
-    };
-  }
-
   componentWillMount() {
     this.init(this.props, this.state);
   }
@@ -43,20 +36,7 @@ class Work extends Component {
   }
 
   componentDidMount() {
-    this.computeContainerWidth();
-  }
-
-  componentDidUpdate() {
-    this.computeContainerWidth();
     this.computeTmpPt();
-  }
-
-  computeContainerWidth() {
-    const {width} = this.state;
-    const panel = ReactDOM.findDOMNode(this.refs.panel);
-    if (width === undefined && panel) {
-      this.setState({width: panel.clientWidth - 30});
-    }
   }
 
   computeTmpPt() {
@@ -68,6 +48,7 @@ class Work extends Component {
       }
     }
   }
+
   metric([x1, y1], [x2, y2]) {
     const dx = x2 - x1, dy = y2 - y1;
     return Math.sqrt(dx * dx + dy * dy);
@@ -125,39 +106,81 @@ class Work extends Component {
 
     const childProps = Object.assign(props, this.state, this.props, {ui: true});
 
+    const {width, height} = this.props;
+    const {tileWidth, tileHeight} = this.props;
+    const hCount = Math.ceil(width / tileWidth),
+          vCount = Math.ceil(height / tileHeight);
+    const tileAt = (x, y) => {
+      const trans = "translate(" + x + "," + y + ")";
+      return (
+        <Tile key={trans} transform={trans} {...childProps} />
+      );
+    };
+
+    var children = [];
+    for (var y = 0; y < vCount; ++y) {
+      for (var x = 0; x < hCount; ++x) {
+        children.push(tileAt(x * tileWidth, y * tileHeight));
+      }
+    }
+
+    return (
+      <svg width={width} height={height} ref="svg">
+        <rect width={width} height={height} stroke="black" fill="none" />
+        {children}
+      </svg>
+    );
+  }
+}
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: undefined
+    };
+    this.computeContainerWidth = this.computeContainerWidth.bind(this);
+  }
+
+  componentDidMount() {
+    this.computeContainerWidth();
+    // window.addEventListener('resize', this.computeContainerWidth); // not working--cbb 2017-08-23
+  }
+
+  componentWillUnmount() {
+    // window.removeEventListener('resize', this.computeContainerWidth);
+  }
+
+  componentDidUpdate() {
+    this.computeContainerWidth();
+  }
+
+  computeContainerWidth() {
+    const {width} = this.state;
+    const panel = ReactDOM.findDOMNode(this.refs.panel);
+    if (panel) {
+      const newWidth = panel.clientWidth - 30;
+      if (width !== newWidth) {
+        this.setState({width: newWidth});
+      }
+    }
+  }
+
+  render() {
     const {width} = this.state;
     if (width) {
       const height = 0.75 * width;
-      const {tileWidth, tileHeight} = this.props;
-      const hCount = Math.ceil(width / tileWidth),
-            vCount = Math.ceil(height / tileHeight);
-      const tileAt = (x, y) => {
-        const trans = "translate(" + x + "," + y + ")";
-        return (
-          <Tile key={trans} transform={trans} {...childProps} />
-        );
-      };
-
-      var children = [];
-      for (var y = 0; y < vCount; ++y) {
-        for (var x = 0; x < hCount; ++x) {
-          children.push(tileAt(x * tileWidth, y * tileHeight));
-        }
-      }
 
       return (
         <Grid >
           <Row>
             <Col sm={12} md={8}>
               <Panel>
-                <svg width={width} height={height} ref="svg">
-                  <rect width={width} height={height} stroke="black" fill="none" />
-                  {children}
-                </svg>
+                <Work width={width} height={height} {...this.props}/>
               </Panel>
             </Col>
             <Col sm={4}>
-              <Controls {...childProps} />
+              <Controls {...this.props} />
             </Col>
           </Row>
         </Grid>
@@ -177,4 +200,4 @@ class Work extends Component {
   }
 }
 
-export default withRouter(Work);
+export default withRouter(Container);
